8cd7dd8f3bd355f4a58d5621cde36dc437484c2c
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-04-22 21:49:48 +0200

    Improved test case

--- examples/plot.jl
+++ examples/plot.jl
@@ -1,4 +1,4 @@
-using KiteViewers, KiteModels
+using KiteViewers, KiteModels, KitePodModels
 viewer = Viewer3D()
 init_system(viewer.scene3D)
 
@@ -10,7 +10,7 @@ if ! @isdefined kps4; const kps4 = Model(kcu); end
 
 # the following values can be changed to match your interest
 dt = 0.05
-TIME = 60
+TIME = 40
 STEPS = Int64(round(TIME/dt))
 FRONT_VIEW = false
 ZOOM = true
@@ -22,18 +22,25 @@ function plot2d(pos, reltime; zoom=ZOOM, front=FRONT_VIEW, segments=se().segment
     update_points(viewer.scene3D, pos, segments, scale)
 end 
 
-function simulate(integrator, steps, plot=false)
+function simulate(integrator, steps)
     start = integrator.p.iter
     for i in 1:steps
+        if steps == 300
+            KitePodModels.set_depower_steering(kcu, 0.25, 0.0)
+        elseif steps == 301
+            KitePodModels.set_depower_steering(kcu, 0.26, -0.0)
+        elseif steps == 302
+            KitePodModels.set_depower_steering(kcu, 0.25, 0.0)
+        end
         KiteModels.next_step!(kps4, integrator, dt=dt)     
         reltime = i*dt
         plot2d(kps4.pos, reltime; zoom=ZOOM, front=FRONT_VIEW, segments=se().segments) 
-        # sleep(dt)         
+        sleep(dt/5)         
     end
     (integrator.p.iter - start) / steps
 end
 
 integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.04, prn=STATISTIC)
 
-av_steps = simulate(integrator, STEPS, true)
+av_steps = simulate(integrator, STEPS)
 nothing
