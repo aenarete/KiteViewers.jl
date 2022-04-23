516b0cdbf46aae0542d9ad24420b5208e0695b0b
Uwe Fechner <fechner@aenarete.eu>
2022-04-23 12:52:47 +0200

    Improve example plot.jl

--- examples/plot.jl
+++ examples/plot.jl
@@ -11,31 +11,51 @@ if ! @isdefined kps4; const kps4 = Model(kcu); end
 # the following values can be changed to match your interest
 dt = 0.05
 TIME = 40
+TIME_LAPSE_RATIO = 10
 STEPS = Int64(round(TIME/dt))
 FRONT_VIEW = false
 ZOOM = true
 STATISTIC = false
 # end of user parameter section #
 
+# finish: time in seconds since epoch
+@inline function wait_until(finish)
+    delta = 0.0002
+    if finish - 0.002 > time()
+        sleep(finish - time() - 0.001)
+    end
+    # sleep 
+    while finish - delta > time()
+        Base.Libc.systemsleep(delta)
+    end
+    # busy waiting
+    while finish > time()-0.95e-6
+    end
+    nothing
+end
+
 function plot2d(pos, reltime; zoom=ZOOM, front=FRONT_VIEW, segments=se().segments)
     scale=0.1
-    update_points(viewer.scene3D, pos, segments, scale)
+    update_points(viewer.scene3D, pos, segments, scale, reltime)
 end 
 
 function simulate(integrator, steps)
     start = integrator.p.iter
+    start_time = time()
     for i in 1:steps
         if steps == 300
-            KitePodModels.set_depower_steering(kcu, 0.25, 0.0)
+            set_depower_steering(kcu, 0.25, 0.01)
         elseif steps == 301
-            KitePodModels.set_depower_steering(kcu, 0.26, -0.0)
+            set_depower_steering(kcu, 0.25, 0.01)
         elseif steps == 302
-            KitePodModels.set_depower_steering(kcu, 0.25, 0.0)
+            set_depower_steering(kcu, 0.25, 0.01)
         end
+        # KitePodModels.on_timer(kcu, dt)
         KiteModels.next_step!(kps4, integrator, dt=dt)     
         reltime = i*dt
         plot2d(kps4.pos, reltime; zoom=ZOOM, front=FRONT_VIEW, segments=se().segments) 
-        sleep(dt/5)         
+        # sleep(dt/5)    
+        wait_until(start_time+i*dt/TIME_LAPSE_RATIO)     
     end
     (integrator.p.iter - start) / steps
 end
