7fd94d395e4d290df0990950abb1260d535ad64a
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-04-25 01:49:20 +0200

    use default config files

--- src/KiteViewers.jl
+++ src/KiteViewers.jl
@@ -6,6 +6,9 @@ using KiteUtils
 export Viewer3D                                                  # types
 export update_points                                             # functions
 
+datapath = joinpath(dirname(dirname(pathof(KiteViewers))), "data")
+KiteUtils.set_data_path(datapath)
+
 include("viewer3D.jl")
 include("common.jl")
 
