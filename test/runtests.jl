bb6da411a0368818444be4c7be40eb4af269c703
Uwe Fechner <fechner@aenarete.eu>
2022-05-17 01:25:00 +0200

    Update settings, fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,9 @@ include("test_steering.jl")
     segments=se().segments
     pos_kite   = kps4.pos[segments+1] # well, this is the position of the pod...
     elevation = calc_elevation(pos_kite)
-    @test isapprox(rad2deg(elevation), 70.31815477683752, rtol=1e-3)
+    @test isapprox(rad2deg(elevation), 69.49651592080296, rtol=1e-3)
     azimuth = azimuth_east(pos_kite)
-    @test isapprox(rad2deg(azimuth), 9.18521251498438, rtol=2e-2)
+    @test isapprox(rad2deg(azimuth), 2.2941958048609155, rtol=2e-2)
     force = winch_force(kps4) 
-    @test isapprox(force, 495.7191503651492, rtol=1e-2)
+    @test isapprox(force, 461.637574830213, rtol=1e-2)
 end
