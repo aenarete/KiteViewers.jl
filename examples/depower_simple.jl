124cc6afc1d1adea57e0a797ade325293eab094c
Uwe Fechner <fechner@aenarete.eu>
2022-05-23 14:00:32 +0200

    Add deadline miss counting

--- examples/depower_simple.jl
+++ examples/depower_simple.jl
@@ -12,25 +12,30 @@ if ! @isdefined kcu;  const kcu = KCU(se());   end
 if ! @isdefined kps4; const kps4 = Model(kcu); end
 
 # the following values can be changed to match your interest
-dt = 0.05
-TIME = 50
-TIME_LAPSE_RATIO = 5
-STEPS = Int64(round(TIME/dt))
+const dt = 0.05
+const TIME = 50
+const TIME_LAPSE_RATIO = 5
+const STEPS = Int64(round(TIME/dt))
 STATISTIC = false
 SHOW_KITE = true
-PLOT_PERFORMANCE = false
+PLOT_PERFORMANCE = true
 # end of user parameter section #
 
 if Model==KPS3 SHOW_KITE = true end
 
-if ! @isdefined time_vec; const time_vec = zeros(div(STEPS, TIME_LAPSE_RATIO)); end
+if ! @isdefined time_vec_gc; const time_vec_gc = zeros(STEPS); end
+if ! @isdefined time_vec_sim; const time_vec_sim = zeros(STEPS); end
+if ! @isdefined time_vec_tot; const time_vec_tot = zeros(div(STEPS, TIME_LAPSE_RATIO)); end
 if ! @isdefined viewer; const viewer = Viewer3D(SHOW_KITE); end
 
 function simulate(integrator, steps)
     start = integrator.p.iter
     start_time_ns = time_ns()
     time_ = 0.0
+    j=0; k=0
     KiteViewers.clear_viewer(viewer)
+    GC.gc()
+    max_time = 0
     for i in 1:steps
         iter = kps4.iter
         if i == 300
@@ -38,17 +43,42 @@ function simulate(integrator, steps)
         elseif i == 640
             set_depower_steering(kps4.kcu, 0.35, 0.0)    
         end
-        KiteModels.next_step!(kps4, integrator, dt=dt)     
-        if mod(i, TIME_LAPSE_RATIO) == 0 || i == steps
-            update_system(viewer, SysState(kps4); scale = 0.08, kite_scale=3.0)
-            wait_until(start_time_ns + dt*1e9, always_sleep=true) 
-            start_time_ns = time_ns()
-            time_vec[div(i, TIME_LAPSE_RATIO)]=time_/(TIME_LAPSE_RATIO*dt)*100.0
-            time_ = 0.0
+        t_sim = @elapsed KiteModels.next_step!(kps4, integrator, dt=dt)
+        t_gc = 0.0
+        if t_sim < 0.08*dt
+            t_gc = @elapsed GC.gc(false)
         end
-        time_ += (kps4.iter - iter)*1.28e-6
+        t_show = 0.0
+        if mod(i, TIME_LAPSE_RATIO) == 0 || i == steps
+            t_show = @elapsed update_system(viewer, SysState(kps4); scale = 0.08, kite_scale=3.0)
+            end_time_ns = time_ns()
+            wait_until(start_time_ns + dt*1e9, always_sleep=true)
+            mtime = 0
+            if i > 10/dt 
+                # if we missed the deadline by more than 2 ms
+                mtime = time_ns() - start_time_ns
+                if mtime > dt*1e9 + 2e6
+                    print(".")
+                    j += 1
+                end
+                k +=1
+            else
+                t_show = 0.0
+            end
+            if mtime > max_time
+                max_time = mtime
+            end
+            time_tot = end_time_ns - start_time_ns
+            start_time_ns = time_ns()
+            time_vec_tot[div(i, TIME_LAPSE_RATIO)] = time_tot/1e9/dt*1000*dt
+        end
+      
+        time_vec_gc[i]=t_gc/dt*100.0
+        time_vec_sim[i]=t_sim/dt*100.0
         if viewer.stop break end
     end
+    misses=j/k * 100
+    println("\nMissed the deadline for $(round(misses, digits=2)) %. Max time: $(round((max_time*1e-6), digits=1)) ms")
     (integrator.p.iter - start) / steps
 end
 
@@ -72,4 +102,10 @@ end
 toc()
 play()
 stop(viewer)
-nothing
+if PLOT_PERFORMANCE
+    using Plots
+    plt=plot(range(dt,TIME,step=dt), time_vec_gc, ylabel="time [%]", xlabel="Simulation time [s]", label="GC time")
+    plt=plot!(range(dt,TIME,step=dt), time_vec_sim, label="sim_time")
+    plt=plot!(range(dt,TIME,step=dt), time_vec_sim.+time_vec_gc, label="total_time")
+    plt2=plot(range(3*TIME_LAPSE_RATIO*dt,TIME,step=dt*TIME_LAPSE_RATIO), time_vec_tot[3:end],  xlabel="Simulation time [s]", ylabel="time per frame [ms]", legend=false)
+end
