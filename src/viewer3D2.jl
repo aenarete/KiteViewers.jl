b5210844e501918816c8b11a66eb8e69eb3ebb4f
Uwe Fechner <fechner@aenarete.eu>
2024-03-14 10:28:00 +0100

    All helpers work

--- src/viewer3D2.jl
+++ src/viewer3D2.jl
@@ -104,7 +104,7 @@ function set_status(kv::AKV, status_text)
     status[] = status_text
 end
 
-# function Viewer3D(show_kite=true, autolabel="Autopilot") 
+function Viewer3D(show_kite=true, autolabel="Autopilot") 
 #     scene, layout = layoutscene(resolution = (840, 900), backgroundcolor = RGBf(0.7, 0.8, 1))
 #     scene3D = LScene(scene, scenekw = (show_axis=false, limits = Rect(-7,-10.0,0, 11,10,11), resolution = (800, 800)), raw=false)
 #     create_coordinate_system(scene3D)
@@ -168,7 +168,7 @@ end
 #     end
 #     status[] = "Stopped"
 #     return s
-# end
+end
 
 # function save_png(viewer; filename="video", index = 1)
 #     buffer = IOBuffer()
