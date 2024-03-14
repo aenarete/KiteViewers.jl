64611bf5c4e4bd17b6a4f6c43d0c122ce82af5be
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-14 13:48:31 +0100

    Cleanup

--- src/viewer3D2.jl
+++ src/viewer3D2.jl
@@ -124,9 +124,8 @@ function Viewer3D(show_kite=true, autolabel="Autopilot")
 
     fig = Figure(size=(840, 900), backgroundcolor=RGBf(0.7, 0.8, 1))
     sub_fig = fig[1,1]
-    scene2D = LScene(fig[3,1], show_axis=false, height=15)
+    scene2D = LScene(fig[3,1], show_axis=false, height=16)
     scene3D = LScene(sub_fig, show_axis=false, scenekw=(limits=Rect(-7,-10.0,0, 11,10,11), size=(800, 800)))
-    # hidedecorations!(scene3D.sub_fig, grid = false)
 
     create_coordinate_system(scene3D)
     cam = cameracontrols(scene3D.scene)
