fc7fcbf20369c6567096e859f3fd46f282980d82
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-18 13:21:28 +0100

    add basic examples

---
+++ examples/basic_1p.jl
@@ -0,0 +1,6 @@
+using KiteViewers, KiteUtils
+viewer=Viewer3D(true);
+segments=6
+state=demo_state(segments+1)
+update_system(viewer, state)
+nothing
---
+++ examples/basic_4p.jl
@@ -0,0 +1,6 @@
+using KiteViewers, KiteUtils
+viewer=Viewer3D(true);
+segments=6
+state=demo_state_4p(segments+1)
+update_system(viewer, state, kite_scale=0.25)
+nothing
