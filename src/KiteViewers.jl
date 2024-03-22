4bf169db279b9f8d9a3527baaf4a42b62f3deace
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-22 18:43:36 +0100

    cleanup

--- src/KiteViewers.jl
+++ src/KiteViewers.jl
@@ -16,11 +16,7 @@ end
 function Viewer3D() 
     fig = Figure(size=(200, 200), backgroundcolor=RGBf(0.7, 0.8, 1))
     sub_fig = fig[1,1]
- 
     fig[2, 1] = buttongrid = GridLayout(tellwidth=false)
-    l_sublayout = GridLayout()
-    fig[1:3, 1] = l_sublayout
-    l_sublayout[:v] = [buttongrid]
 
     btn_PLAY_PAUSE  = Button(sub_fig, label = @lift($running ? "PAUSE" : " RUN "))
     btn_AUTO        = Button(sub_fig, label = "Autolabel")
