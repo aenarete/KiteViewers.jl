31f36ceaf93dac8229b0deea3295a501323be4fb
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-22 17:47:36 +0100

    cleanup

--- src/KiteViewers.jl
+++ src/KiteViewers.jl
@@ -25,13 +25,7 @@ include("common.jl")
 end
 
 @setup_workload begin
-	# Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
-	# precompile file and potentially make loading faster.
-	# list = [OtherType("hello"), OtherType("world!")]
-	set_data_path()
 	@compile_workload begin
-		# all calls in this block will be precompiled, regardless of whether
-		# they belong to your package or not (on Julia 1.8 and higher)
 		viewer=Viewer3D()
         close(viewer.screen)
         nothing
