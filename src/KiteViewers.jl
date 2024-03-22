0d7066af9770904c11b4619a025ce858788cde3d
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-22 18:03:08 +0100

    cleanup

--- src/KiteViewers.jl
+++ src/KiteViewers.jl
@@ -3,21 +3,36 @@ module KiteViewers
 using PrecompileTools: @setup_workload, @compile_workload 
 using GeometryBasics, Rotations, GLMakie, FileIO, LinearAlgebra, Printf, Parameters, Reexport
 import GeometryBasics:Point3f, GeometryBasics.Point2f
-using KiteUtils
 
-export Viewer3D, AbstractKiteViewer, AKV                        # types
-export clear_viewer, update_system, save_png, stop, set_status  # functions
-@reexport using GLMakie: on
+export Viewer3D
 
-const KITE_SPRINGS = 8 
+const    running   = Observable(false)
 
-function __init__()
-    if isdir(joinpath(pwd(), "data")) && isfile(joinpath(pwd(), "data", "system.yaml"))
-        set_data_path(joinpath(pwd(), "data"))
-    end
+mutable struct Viewer3D
+    fig::Figure
+    screen::GLMakie.Screen
+    btn_PLAY::Button
+    btn_AUTO::Button
 end
 
-include("viewer3D.jl")
+function Viewer3D() 
+    fig = Figure(size=(200, 200), backgroundcolor=RGBf(0.7, 0.8, 1))
+    sub_fig = fig[1,1]
+ 
+    fig[2, 1] = buttongrid = GridLayout(tellwidth=false)
+    l_sublayout = GridLayout()
+    fig[1:3, 1] = l_sublayout
+    l_sublayout[:v] = [buttongrid]
+
+    btn_PLAY_PAUSE  = Button(sub_fig, label = @lift($running ? "PAUSE" : " RUN "))
+    btn_AUTO        = Button(sub_fig, label = "Autolabel")
+    
+    buttongrid[1, 1:2] = [btn_PLAY_PAUSE, btn_AUTO]
+    gl_screen = display(fig)
+
+    Viewer3D(fig, gl_screen, btn_PLAY_PAUSE, btn_AUTO)
+end
+
 
 @setup_workload begin
 	@compile_workload begin
