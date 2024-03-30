70e800f60e5298beadae3ee5507bf3b308491f30
Uwe Fechner <fechner@aenarete.eu>
2024-03-30 11:09:43 +0100

    new testcase

---
+++ mwes/mwe_06.jl
@@ -0,0 +1,9 @@
+using KiteViewers, KiteUtils
+set = deepcopy(se())
+set.segments=12
+viewer=Viewer3D(set, true; precompile=true)
+segments=set.segments
+state=demo_state_4p(segments+1)
+update_system(viewer, state, kite_scale=0.25)
+sleep(5)
+close(viewer.screen)
