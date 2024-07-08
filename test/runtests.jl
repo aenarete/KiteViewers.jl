f60f0888a3af0db373da61a0f480d91ceafc3166
Uwe Fechner <fechner@aenarete.eu>
2024-07-08 12:25:58 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,15 +5,15 @@ set_data_path()
 using Test
 
 cd("..")
-include("test_steering.jl")
+include("test_parking.jl")
 
 @testset "KiteViewers.jl" begin
     segments=se().segments
     pos_kite   = kps4.pos[segments+1] # well, this is the position of the pod...
     elevation = calc_elevation(pos_kite)
-    @test isapprox(rad2deg(elevation), 65.14614262632229, rtol=1e-2)
+    @test isapprox(rad2deg(elevation), 69.51421682882923, rtol=1e-2)
     azimuth = azimuth_east(pos_kite)
-    @test isapprox(rad2deg(azimuth), 33.24375094213839, rtol=2e-2, atol=10)
+    @test isapprox(rad2deg(azimuth), 0.1, rtol=2e-2, atol=10)
     force = winch_force(kps4) 
-    @test isapprox(force, 593.6905956072441, rtol=2e-2)
+    @test isapprox(force, 461.0137819702637, rtol=2e-2)
 end
