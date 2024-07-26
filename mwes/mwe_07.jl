664fcab4782aa26f6ba0de7c407a3355ec52bc66
Uwe Fechner <fechner@aenarete.eu>
2024-07-26 10:18:53 +0200

    update mwe_07.jl

--- mwes/mwe_07.jl
+++ mwes/mwe_07.jl
@@ -1,5 +1,5 @@
 # http example
-using HTTP, JSON3, KiteUtils
+using HTTP, JSON3, StructTypes, KiteUtils
 
 url = "http://localhost:8080"
 
@@ -12,7 +12,7 @@ function init()
     end
 end
 
-function sys_state()
+function get_sys_state()
     response = HTTP.get(url*"/sys_state")
     if response.status == 200
         return JSON3.read(response.body)
@@ -21,5 +21,19 @@ function sys_state()
     end
 end
 
-# TODO: convert JSON into a SystemState object
+function get_sys_state2()
+    response = HTTP.get(url*"/sys_state")
+    if response.status == 200
+        return (response.body)
+    else
+        return nothing
+    end
+end
 
+# TODO: convert JSON into a SysState object
+
+# JSON3.@generatetypes ss
+# StructTypes.StructType(::Type{SysState{P}}) = StructTypes.Struct()
+# ss = get_sys_state()
+# P = length(ss.Z)
+# sys_state = JSON3.read(get_sys_state(), SysState{P})
