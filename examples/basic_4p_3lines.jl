f1a8fb4ecd428f60a50fc8b84e2c722f6d205607
1-Bart-1 <bart@vandelint.net>
2024-08-06 08:15:44 +0200

    fix points number

--- examples/basic_4p_3lines.jl
+++ examples/basic_4p_3lines.jl
@@ -1,6 +1,6 @@
 using KiteViewers, KiteUtils
 viewer::Viewer3D = Viewer3D(true);
 segments=6
-state=KiteUtils.demo_state_4p_3lines(segments*3+6)
+state=KiteUtils.demo_state_4p_3lines(segments+1)
 update_system(viewer, state, scale=0.08, kite_scale=4.0)
 nothing
