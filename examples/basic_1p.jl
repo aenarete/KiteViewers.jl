1f0ec9cf15d61aea06680072ffcea38f8d85cd92
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-02 21:17:54 +0100

    fix first example

--- examples/basic_1p.jl
+++ examples/basic_1p.jl
@@ -2,5 +2,5 @@ using KiteViewers, KiteUtils
 viewer::Viewer3D = Viewer3D(true);
 segments=6
 state=demo_state(segments+1)
-update_system(viewer, state)
+update_system(viewer, state; ned=false)
 nothing
