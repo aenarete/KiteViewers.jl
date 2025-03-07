7435d67867d3bed51b6532fa492cfe0bcded52cc
Uwe Fechner <fechner@aenarete.eu>
2025-03-07 10:44:00 +0100

    use name Manifest-v1.10.toml

--- examples/update_manifest.jl
+++ examples/update_manifest.jl
@@ -1,7 +1,7 @@
 using TestEnv; pr=TestEnv.activate()
 using KiteViewers, KiteModels
 src = joinpath(dirname(pr), "Manifest.toml")
-dest = joinpath(pwd(), "Manifest.toml")
+dest = joinpath(pwd(), "Manifest-v1.10.toml")
 cp(src, dest; force=true)
 using Pkg
 Pkg.activate(".")
