f0e8241b8cef29e5b379beeb1ad9deac2ba5b901
Uwe Fechner <fechner@aenarete.eu>
2025-09-29 15:52:25 +0200

    Update script update_manifest.jl

--- examples/update_manifest.jl
+++ examples/update_manifest.jl
@@ -1,7 +1,15 @@
 using TestEnv; pr=TestEnv.activate()
 using KiteViewers, KiteModels
 src = joinpath(dirname(pr), "Manifest.toml")
-dest = joinpath(pwd(), "Manifest-v1.10.toml")
+if VERSION.minor==10
+    dest = joinpath(pwd(), "Manifest-v1.10.toml")
+elseif VERSION.minor==11
+    dest = joinpath(pwd(), "Manifest-v1.11.toml")
+elseif VERSION.minor==12
+    dest = joinpath(pwd(), "Manifest-v1.12.toml")
+else
+    error("This script only supports Julia 1.10, 1.11, and 1.12")
+end
 cp(src, dest; force=true)
 using Pkg
 Pkg.activate(".")
